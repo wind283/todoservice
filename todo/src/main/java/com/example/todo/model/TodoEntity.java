9a418317caba5ced6de804bed0f34995222b115c
lithe283 <107051024+wind283@users.noreply.github.com>
2024-05-29 12:16:22 +0900

    Add files via upload

---
+++ todo/src/main/java/com/example/todo/model/TodoEntity.java
@@ -0,0 +1,28 @@
+package com.example.todo.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "Todo")
+public class TodoEntity {
+	@Id
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid",strategy="uuid")
+	private String id;
+	private String userId;
+	private String title;
+	private boolean done;
+}
